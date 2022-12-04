72ef2cd08ef6fd619e44a1da7ce76cfab3c10219
min33sky <min33sky@naver.com>
2022-12-04 21:29:47 +0900

    feat: implements ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import ImageUpload from './components/ImageUpload';
+import ResizeControl from './components/ResizeControl';
 
 export default function App() {
   return (
     <main className="grid h-screen place-items-center bg-gradient-to-tr from-slate-800 to-slate-600">
-      <div className="flex w-10/12 max-w-2xl flex-col space-y-6 rounded-lg bg-slate-100 px-6 py-8 shadow-lg">
+      <div className="flex w-10/12 max-w-2xl flex-col space-y-6 rounded-lg bg-slate-100 px-5 py-10 shadow-lg">
         <h1 className="text-center text-2xl font-bold">이미지 리사이저</h1>
         {/* 이미지 업로드 컴포넌트 */}
         <ImageUpload />
 
         {/* 이미지 리사이즈 컴포넌트 */}
+        <ResizeControl />
 
         {/* 다운로드 버튼 */}
+        <button className="bg-slate-700 py-3 text-lg tracking-widest text-slate-100 transition hover:bg-slate-800">
+          다운로드
+        </button>
       </div>
     </main>
   );
---
+++ src/components/ResizeControl.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+export default function ResizeControl() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [isRatioFixed, setIsRatioFixed] = useState(true);
+  const [quality, setQuality] = useState(50);
+
+  /**
+   * 이미지 너비, 높이를 변경하는 핸들러
+   */
+  const handleSize = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'width') {
+      setWidth(Number(value));
+    } else if (name === 'height') {
+      setHeight(Number(value));
+    }
+  };
+
+  const handleClickRatioFixed = () => {
+    setIsRatioFixed(!isRatioFixed);
+  };
+
+  /**
+   * 이미지 퀄리티를 변경하는 핸들러
+   */
+  const handleQualityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuality(Number(e.target.value));
+  };
+
+  console.log(width, height, isRatioFixed, quality);
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-4 py-4 text-xl md:grid-cols-8">
+        <div className="flex items-center space-x-3  md:col-span-3">
+          <label htmlFor="width" className="flex-shrink-0">
+            너비
+          </label>
+          <input
+            type="number"
+            id="width"
+            name="width"
+            onChange={handleSize}
+            className="w-full rounded-md border border-slate-300 p-2 outline-none"
+          />
+        </div>
+
+        <div className="flex items-center space-x-3 md:col-span-3">
+          <label htmlFor="height" className="flex-shrink-0">
+            높이
+          </label>
+          <input
+            type="number"
+            id="height"
+            name="height"
+            onChange={handleSize}
+            className="w-full rounded-md border border-slate-300 p-2 outline-none"
+          />
+        </div>
+
+        <div className="flex items-center space-x-3 py-2 md:col-span-2">
+          <label htmlFor="aspect" className="flex-shrink-0 cursor-pointer">
+            비율 유지
+          </label>
+          <input
+            type="checkbox"
+            id="aspect"
+            className="h-6 w-6 cursor-pointer"
+            checked={isRatioFixed}
+            onChange={handleClickRatioFixed}
+          />
+        </div>
+
+        <div className="flex items-center space-x-3 py-2 md:col-span-full">
+          <label htmlFor="quality" className="flex-shrink-0 cursor-pointer">
+            이미지 퀄리티
+          </label>
+          <input
+            title={`${quality}%`}
+            type="range"
+            id="quality"
+            className="w-full cursor-move"
+            onChange={handleQualityChange}
+            value={quality}
+          />
+          <div className="">{quality}%</div>
+        </div>
+      </div>
+    </div>
+  );
+}
